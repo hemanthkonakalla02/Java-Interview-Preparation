aa3e63c38a93f0bfdb07d78dfddf14ef4f3f5494
HemanthKumarKonakalla <hemanthkonakalla02@gmail.com>
2026-09-18 22:41:36 +0530

    String Coding Practice

--- Java_Interview_Preparation_Day_02/Java_Coding_Questions&Answers/Strings/RemoveDuplicateCharacters.java
+++ Java_Interview_Preparation_Day_02/Java_Coding_Questions&Answers/Strings/RemoveDuplicateCharacters.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class RemoveDuplicateCharacters 
 {
 	public static void main(String[] args) 
-	{
+	{ 
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the String:");
 		String s=sc.next();
