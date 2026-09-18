1feb0443b83ca106dbe4f3f673f2f20cd9550837
HemanthKumarKonakalla <hemanthkonakalla02@gmail.com>
2026-09-18 22:40:52 +0530

    String Coding Practice

--- Java_Interview_Preparation_Day_02/Java_Coding_Questions&Answers/Strings/FirstNonRepeatedCharacter.java
+++ Java_Interview_Preparation_Day_02/Java_Coding_Questions&Answers/Strings/FirstNonRepeatedCharacter.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class FirstNonRepeatedCharacter 
 {
-	public static void main(String[] args) 
+	public static void main(String[] args)  
 	{
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the string:");
