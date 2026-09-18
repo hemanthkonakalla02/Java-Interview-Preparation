d9005f3a3bd5a9b5bbd52ff54a0ce1cb548c49b4
HemanthKumarKonakalla <hemanthkonakalla02@gmail.com>
2026-09-18 22:39:58 +0530

    String Coding Practice

--- Java_Interview_Preparation_Day_02/Java_Coding_Questions&Answers/Strings/CountVowelsAndConsonants.java
+++ Java_Interview_Preparation_Day_02/Java_Coding_Questions&Answers/Strings/CountVowelsAndConsonants.java
@@ -11,7 +11,7 @@ public class CountVowelsAndConsonants
 		String s=sc.next();
 		int vowels=0;
 		int consonants=0;
-		char[] charArray = s.toCharArray();
+		char[] charArray = s.toCharArray(); 
 		for(int i=0;i<=charArray.length-1;i++)//6<=6
 		{
 			if(s.charAt(i)=='a' || s.charAt(i)=='e' || s.charAt(i)=='i'||s.charAt(i)=='u')
