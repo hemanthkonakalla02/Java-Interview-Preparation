f30c736ff6f389a1d3ac016e0ca5f856f65821fe
HemanthKumarKonakalla <hemanthkonakalla02@gmail.com>
2026-09-18 22:40:28 +0530

    String Coding Practice

--- Java_Interview_Preparation_Day_02/Java_Coding_Questions&Answers/Strings/CountWordsInString.java
+++ Java_Interview_Preparation_Day_02/Java_Coding_Questions&Answers/Strings/CountWordsInString.java
@@ -6,7 +6,7 @@ public class CountWordsInString
 {
 	public static void main(String[] args) 
 	{
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in); 
 		System.out.println("Enter a String:");
 		String s =sc.nextLine();
 		int count=0;
