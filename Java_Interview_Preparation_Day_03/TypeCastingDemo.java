09ddd24fe72b561708be87338edb0272e847169f
HemanthKumarKonakalla <hemanthkonakalla02@gmail.com>
2026-09-19 16:45:12 +0530

    Day 03: JavaFundamentals and coding

---
+++ Java_Interview_Preparation_Day_03/TypeCastingDemo.java
@@ -0,0 +1,46 @@
+package com.java.standard.edition.typecasting;
+
+public class TypeCastingDemo 
+{
+	public static void main(String[] args) 
+	{
+		//implicit typecasting
+		int a=1000;
+		long b=a;
+		
+		System.out.println("The value stored in variable a is :"+a); //1000
+		System.out.println("The value stored in variable b is :"+b); //1000
+		
+		System.out.println("=======================================================");
+		//explicit typecasting
+		short x=100;
+		byte y=(byte)x;
+		
+		System.out.println("The value stored in variable x is:"+x);
+		System.out.println("The value stored in variable y is :"+y);
+		
+		System.out.println("=======================================================");
+		//dataloss during explicit typecasting
+		float p=76.2f;
+		int q=(int)p;
+		System.out.println("The value stored in variable p is :"+p);//76.2
+		System.out.println("The value stored in variable q is :"+q);//76
+		
+		System.out.println("=======================================================");
+		//converting the char datatype into another datatype
+		char x1='A';
+		int y1=(int)x1;
+		
+		System.out.println("The value stored in variable x1 is :"+x1);//A
+		System.out.println("The value stored in variable y1 is :"+y1);//65
+		
+		System.out.println("=======================================================");
+
+		short a1=131;
+		byte b1=(byte)a1;  //-128 to +127
+		System.out.println("The value stored in variable a1 is :"+a1);//128
+		System.out.println("The value stored in variable b1 is :"+b1);//-125
+		
+	}
+
+}
---
+++ Java_Interview_Preparation_Day_03/WrapperClassDemo.java
@@ -0,0 +1,41 @@
+package com.java.standard.edition.wrapperclass;
+
+public class WrapperClassDemo 
+{
+	public static void main(String[] args) 
+	{
+		Boolean a =true;
+		Character b ='k';
+		Byte c=100;
+		Short d =30000;
+		Integer e =25000;
+		Long f=1000000L;
+		Float g=76.2f;
+		Double h = 78.90876;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		System.out.println(h);
+		
+		System.out.println("=======================================================================");
+		
+		//Autoboxing
+		int x=100;
+		Integer y=x;
+		System.out.println("The value stored in variable x is :"+x);
+		System.out.println("The value stored in variable y is :"+y);
+		
+		System.out.println("=======================================================================");
+
+		//Unboxing
+		int p=y;
+		System.out.println("The value stored in variable p is :"+p);
+		
+	}
+
+}
