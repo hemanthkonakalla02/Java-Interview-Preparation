42570f932b3f703bed30d5f18645a976e4b33ba1
HemanthKumarKonakalla <hemanthkonakalla02@gmail.com>
2026-09-19 16:46:39 +0530

    Notes and Example for Scanner

--- Java_Interview_Preparation_Day_03/ScannerDemo.java
+++ Java_Interview_Preparation_Day_03/ScannerDemo.java
@@ -6,7 +6,7 @@ public class ScannerDemo
 {
 	public static void main(String[] args) 
 	{
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in); 
 		
 		System.out.println("Enter the marriage status:");
 		boolean a =sc.nextBoolean();
