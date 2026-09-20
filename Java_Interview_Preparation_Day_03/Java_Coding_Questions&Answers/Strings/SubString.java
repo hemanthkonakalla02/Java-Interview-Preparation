21b00e50d576bb7e286503ffff72aecd9b6b3cb1
HemanthKumarKonakalla <hemanthkonakalla02@gmail.com>
2026-09-20 22:25:23 +0530

    SubString

--- Java_Interview_Preparation_Day_03/Java_Coding_Questions&Answers/Strings/SubString.java
+++ Java_Interview_Preparation_Day_03/Java_Coding_Questions&Answers/Strings/SubString.java
@@ -7,7 +7,7 @@ public class SubString
 	public static void main(String[] args) 
 	{
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the String:");
+		System.out.println("Enter the String:"); 
 		String s=sc.next();
 		
 		for(int i=0;i<=s.length()-1;i++)//3<=3
