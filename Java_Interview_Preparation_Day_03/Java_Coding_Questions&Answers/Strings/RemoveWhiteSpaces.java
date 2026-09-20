80725d2a673c4e2c32970cb4452d215fdec0ce95
HemanthKumarKonakalla <hemanthkonakalla02@gmail.com>
2026-09-20 22:24:47 +0530

    RemoveWhiteSpaces

--- Java_Interview_Preparation_Day_03/Java_Coding_Questions&Answers/Strings/RemoveWhiteSpaces.java
+++ Java_Interview_Preparation_Day_03/Java_Coding_Questions&Answers/Strings/RemoveWhiteSpaces.java
@@ -5,7 +5,7 @@ public class RemoveWhiteSpaces
 	public static void main(String[] args) 
 	{
 		String s =" Hemanth Kumar Konakalla is Javadev";
-		System.out.println("Before Removing WhiteSpaces:"+s);
+		System.out.println("Before Removing WhiteSpaces:"+s); 
 		s=s.replaceAll("\\s", "");
 		System.out.println("After Removing WhiteSpaces:"+s);
 	}
