379b78bfb7e3b1f145b6820442062f343f35325e
HemanthKumarKonakalla <hemanthkonakalla02@gmail.com>
2026-09-23 21:50:41 +0530

    ArithematicOperators

--- Java_Interview_Preparation_Day_04/operators/ArithematicOperators.java
+++ Java_Interview_Preparation_Day_04/operators/ArithematicOperators.java
@@ -6,7 +6,7 @@ public class ArithematicOperators
 	{
 		System.out.println(10+2);  //12
 		System.out.println(10-2); //8
-		System.out.println(10*2); //20
+		System.out.println(10*2); //20 
 		System.out.println(10/2); //5
 		System.out.println(10%2); //0
 		
